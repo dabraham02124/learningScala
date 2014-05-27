5c13592ef3e1d72e3ef3ebd40908bcb8be5dd70b
Daniel Abraham <abrahamd@akamai.com>
2014-05-27 18:40:13 -0400

    finished exercises

--- chapter2.scala
+++ chapter2.scala
@@ -7,10 +7,31 @@ def loopExample(n : Int) {
 }
 
 def unicodeProduct(s : String) = {
-  var r = 1
+  var r:Long = 1
   for (ch <- s) {
-    println(ch.toInt) 
     r = r * ch
   }
   r
+}
+
+def unicodeProductRecursive(s : String):Long = {
+  if ("".equals(s))
+    return 1L
+  s.take(1).charAt(0) * uPR(s.drop(1))
+}
+
+def exp(x: Int, n: Int):Float = {
+  if (n == 0) {
+    1
+  } else if (n == 1) {
+    x
+  } else if (n < 1) {
+    1 / exp(x, -n)
+  } else if (n%2 == 0) {
+    var n2 = n/2
+    var subP = exp(x,n2)
+    subP * subP
+  } else {
+    x * exp(x,n-1)
+  }
 }
