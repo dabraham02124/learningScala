e2d772bc57243e999d9f18dee14f4c82c2acb5f7
Daniel Abraham <abrahamd@akamai.com>
2014-06-15 18:51:27 -0400

    up through page 35

--- chapter3.scala
+++ chapter3.scala
@@ -28,11 +28,26 @@ val result = for (elem <- a) yield 2 * elem
 val result2 = for (elem <- a if elem % 2 == 0) yield 2 * elem //this one fixes a bug in the book
 a.filter(_ %2 == 0).map(2*_)
 a filter {_%2 == 0} map {2*_}
+Array(1,2,3,4).map(2*_).fold(0){_ + _}
 
+//page 34
+Array(1,7,2,9).sum //yields 19
+Array("Mary","had","a","little","lamb").max //yields little (off of size, I think)
+
+val c = Array(1,7,2,9)
+val cSorted = c.sortWith(_ < _) //it uses sorted in the book, doesn't work in my scala...
+val cSorted2 = c.sorted
+scala.util.Sorting.quickSort(c)  ;c.toArray
+
+b.mkString(" and ")
+b.mkString("<",",",">")
+
+//page 35
+a.toString   //annoying java toString
+b.toString   //useful scala toString because it's not an Array...
 
 
 // exercises
-println
 println
 println
 
